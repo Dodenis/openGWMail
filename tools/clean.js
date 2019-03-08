c32216d1d165350f53cd88a453ca6a192af60ece
Dodenis <ogierdenis.damien@gmail.com>
2019-03-08 13:54:42 +0100

    Update dependancies

--- tools/clean.js
+++ tools/clean.js
@@ -1,14 +1,27 @@
 const { PACKAGE_DIRS } = require('./constants')
 const rimraf = require('rimraf')
+const fs = require('fs')
+
+const rmCustom = function (folder) {
+  const folderContents = fs.readdirSync(folder)
+
+  folderContents.forEach(content => {
+    if (content === '.bin') {
+      rmCustom(folder + '/' + content)
+    } else if (content !== 'rimraf') {
+      rimraf(
+        folder + '/' + content,
+        function (err) {
+          if (err) {
+            console.log(err)
+          }
+        }
+      )
+    }
+  })
+}
 
 PACKAGE_DIRS.forEach((dir) => {
-  console.log('Clean:' + dir)
-  rimraf(
-    dir + '/node_modules/!(rimraf|.bin)',
-    function (err) {
-      if (err) {
-        console.log(err)
-      }
-    }
-  )
+  console.log('Clean: ' + dir)
+  rmCustom(dir + '/node_modules')
 })
